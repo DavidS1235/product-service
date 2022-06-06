b85f3e683d476376d21ab522e3ca2d4d5a913a98
davidsc <davidsilvacordova@gmail.com>
2022-06-05 21:12:54 -0500

    Clase modelo ProductStatus creada

---
+++ src/main/java/com/ds/productservice/model/mongo/ProductStatus.java
@@ -0,0 +1,25 @@
+package com.ds.productservice.model.mongo;
+
+
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection ="STATUS_PRODUCT")
+public class ProductStatus {
+
+    @Id
+    private Integer id;
+    private Integer idProduct;
+    private Integer idSttusPduct;
+    private Integer FlgCurrent;
+    private Date FecBeginning;
+    private Date FecFinnish;
+
+}
