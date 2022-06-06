d350aac11d577d199f78553145fa5bab8415dc42
davidsc <davidsilvacordova@gmail.com>
2022-06-05 21:23:00 -0500

    Clase modelo TypeCurrency creada

---
+++ src/main/java/com/ds/productservice/model/mongo/TypeCurrency.java
@@ -0,0 +1,19 @@
+package com.ds.productservice.model.mongo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection ="TYPE_CURRENCY")
+public class TypeCurrency {
+
+    @Id
+    private Integer id;
+    private String code;
+    private String name;
+
+}
