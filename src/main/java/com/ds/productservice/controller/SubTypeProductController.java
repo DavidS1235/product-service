c505ba097dba20ba59941330c465c3770f42b866
davidsc <davidsilvacordova@gmail.com>
2022-06-07 16:39:26 -0500

    CRUD SubTypeProduct creado

--- src/main/java/com/ds/productservice/controller/SubTypeProductController.java
+++ src/main/java/com/ds/productservice/controller/SubTypeProductController.java
@@ -2,10 +2,10 @@ package com.ds.productservice.controller;
 
 import com.ds.productservice.business.service.SubTypeProductService;
 import com.ds.productservice.document.SubTypeProduct;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -16,9 +16,53 @@ public class SubTypeProductController {
     private SubTypeProductService subTypeProductService;
 
 
+
     @GetMapping("/liststp")
-    public Mono<ResponseEntity<Flux<SubTypeProduct>>> listSubTypes(){
+    public Mono<ResponseEntity<Flux<SubTypeProduct>>> findAll(){
         return Mono.just(ResponseEntity.ok()
-                .body(subTypeProductService.listSubTypeProducts()));
+                .body(subTypeProductService.findAllSubTypeProduct()));
     }
+
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<SubTypeProduct>> findById(@PathVariable String id) {
+        return subTypeProductService.find(id)
+                .map(p -> ResponseEntity.ok()
+                        .body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build()); //404
+    }
+
+    @PostMapping
+    public Mono<ResponseEntity<SubTypeProduct>> create(@RequestBody SubTypeProduct stp) {
+        return subTypeProductService.saveSubTypeProduct(stp)
+                .map(p -> ResponseEntity.created(URI.create("/api/subTypeProduct/".concat(p.getId())))
+                        .body(p)
+                );
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<SubTypeProduct>> update(@RequestBody SubTypeProduct stp, @PathVariable String id){
+        return subTypeProductService.find(id)
+                .flatMap(sub -> {
+                    sub.setCode(stp.getCode());
+                    sub.setName(stp.getName());
+                    sub.setDate(stp.getDate());
+                    return subTypeProductService.saveSubTypeProduct(stp);
+                })
+                .map(p -> ResponseEntity.created(URI.create("/api/subTypeProduct/".concat(p.getId())))
+                        .body(p)
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Object>> delete(@PathVariable String id) {
+        return subTypeProductService.find(id)
+                .flatMap(stp -> {
+                    return subTypeProductService.DeleteSubTypeProduct(stp)
+                            .then(Mono.just(ResponseEntity.noContent().build()));
+                })
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+
 }
