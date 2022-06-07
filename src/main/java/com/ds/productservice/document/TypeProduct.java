63da3a10b149b788bb1a12e49114f39bf8a1f6e8
davidsc <davidsilvacordova@gmail.com>
2022-06-07 11:46:26 -0500

    TypeProduct.java model created

---
+++ src/main/java/com/ds/productservice/document/TypeProduct.java
@@ -0,0 +1,24 @@
+package com.ds.productservice.document;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Document("TYPE_PRODUCT")
+public class TypeProduct {
+
+    @Id
+    private String id;
+    private String code;
+    private String name;
+
+    public TypeProduct(String code, String name) {
+        this.code = code;
+        this.name = name;
+    }
+}
