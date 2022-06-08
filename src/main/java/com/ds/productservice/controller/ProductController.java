be0ae197c702e51c166b1bf99b83e2f498725a2a
davidsc <davidsilvacordova@gmail.com>
2022-06-07 21:25:05 -0500

    Update model ProductController

--- src/main/java/com/ds/productservice/controller/ProductController.java
+++ src/main/java/com/ds/productservice/controller/ProductController.java
@@ -1,9 +1,14 @@
 package com.ds.productservice.controller;
 
 import com.ds.productservice.business.service.ProductService;
+import com.ds.productservice.document.Product;
+import com.ds.productservice.document.SubTypeProduct;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api/product")
@@ -12,7 +17,52 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
+    @GetMapping("/liststp")
+    public Mono<ResponseEntity<Flux<Product>>> findAll(){
+        return Mono.just(ResponseEntity.ok()
+                .body(productService.findAllProduct()));
+    }
 
+    @GetMapping("/{id}")
+    public Mono<ResponseEntity<Product>> findById(@PathVariable String id) {
+        return productService.find(id)
+                .map(p -> ResponseEntity.ok()
+                        .body(p))
+                .defaultIfEmpty(ResponseEntity.notFound().build()); //404
+    }
+
+    @PostMapping()
+    public Mono<ResponseEntity<Product>> create(@RequestBody Product p) {
+        return productService.saveProduct(p)
+                .map(prd -> ResponseEntity.created(URI.create("/api/product/".concat(prd.getId())))
+                        .body(p)
+                );
+    }
+
+    @PutMapping("/{id}")
+    public Mono<ResponseEntity<Product>> update(@RequestBody Product p, @PathVariable String id){
+        return productService.find(id)
+                .flatMap(prd -> {
+                    prd.setIdBank(p.getIdBank());
+                    prd.setTpeCrrency(p.getTpeCrrency());
+                    prd.setIdClient(p.getIdClient());
+                    return productService.saveProduct(p);
+                })
+                .map(pr -> ResponseEntity.created(URI.create("/api/product/".concat(pr.getId())))
+                        .body(pr)
+                )
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public Mono<ResponseEntity<Object>> delete(@PathVariable String id) {
+        return productService.find(id)
+                .flatMap(p -> {
+                    return productService.DeleteProduct(p)
+                            .then(Mono.just(ResponseEntity.noContent().build()));
+                })
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
 
 
 
