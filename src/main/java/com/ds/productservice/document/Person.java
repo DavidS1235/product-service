1664935b31cf938336463df2cfe6d07ee53204c1
davidsc <davidsilvacordova@gmail.com>
2022-06-16 13:13:03 -0500

    Update

--- src/main/java/com/ds/productservice/document/Person.java
+++ src/main/java/com/ds/productservice/document/Person.java
@@ -1,5 +1,8 @@
 package com.ds.productservice.document;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "PERSON")
 public class Person {
 
   private String id;
